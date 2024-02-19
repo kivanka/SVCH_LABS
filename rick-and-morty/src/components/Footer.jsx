0aa8a0abce501c661206d79cd1fea2c16833d15b
Dima <138651222+kivanka@users.noreply.github.com>
2024-02-20 01:08:07 +0300

    feat: add footer component

---
+++ rick-and-morty/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <a href="https://github.com/[your-github]">GitHub</a> | <span>&copy; 2023</span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
