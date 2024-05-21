6a0bc7b384ad190d38509d14a2f2f211e8bf10e4
Ksenya <138651222+kivanka@users.noreply.github.com>
2024-05-21 11:41:48 +0300

    починил

--- repair-tracking/server.js
+++ repair-tracking/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -19,46 +20,6 @@ const writeData = (data) => {
 app.get('/api/data', (req, res) => {
     const data = readData();
     res.json(data);
-});
-
-app.post('/api/data', (req, res) => {
-    const data = { message: 'Hello, World!' };
-    res.json(data);
-});
-
-app.post('/api/submit', (req, res) => {
-    const inputData = req.body;
-    console.log('Received data:', inputData);
-    res.json({ status: 'success', receivedData: inputData });
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/api/format', (req, res) => {
-    const accept = req.headers.accept;
-    const data = readData();
-
-    if (accept.includes('application/xml')) {
-        let xmlData = '<?xml version="1.0" encoding="UTF-8"?><data>';
-        for (const key in data) {
-            xmlData += `<${key}>${data[key]}</${key}>`;
-        }
-        xmlData += '</data>';
-        res.header('Content-Type', 'application/xml');
-        res.send(xmlData);
-    } else if (accept.includes('text/html')) {
-        let htmlData = '<html><body><ul>';
-        for (const key in data) {
-            htmlData += `<li>${key}: ${data[key]}</li>`;
-        }
-        htmlData += '</ul></body></html>';
-        res.header('Content-Type', 'text/html');
-        res.send(htmlData);
-    } else {
-        res.json(data);
-    }
 });
 
 app.post('/api/equipment', (req, res) => {
@@ -95,6 +56,35 @@ app.delete('/api/equipment/:id', (req, res) => {
     } else {
         res.status(404).json({ status: 'error', message: 'Equipment not found' });
     }
+});
+
+app.get('/api/format', (req, res) => {
+    const accept = req.headers.accept;
+    const data = readData();
+
+    if (accept.includes('application/xml')) {
+        let xmlData = '<?xml version="1.0" encoding="UTF-8"?><data>';
+        data.equipment.forEach(item => {
+            xmlData += `<equipment><id>${item.id}</id><name>${item.name}</name><status>${item.status}</status></equipment>`;
+        });
+        xmlData += '</data>';
+        res.header('Content-Type', 'application/xml');
+        res.send(xmlData);
+    } else if (accept.includes('text/html')) {
+        let htmlData = '<html><body><ul>';
+        data.equipment.forEach(item => {
+            htmlData += `<li>ID ${item.id} | ${item.name}: ${item.status}</li>`;
+        });
+        htmlData += '</ul></body></html>';
+        res.header('Content-Type', 'text/html');
+        res.send(htmlData);
+    } else {
+        res.json(data);
+    }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
